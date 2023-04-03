c22017c99621b070810235650925d7c4a30429d1
testers3333 <114099236+testers3333@users.noreply.github.com>
2023-04-03 19:59:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let result = [];
+
 document.addEventListener("DOMContentLoaded", async () => {
     // récupération des éléments html.
     let soumission = document.getElementById("btn"); // récupération du bouton soumission.
@@ -27,10 +29,51 @@ document.addEventListener("DOMContentLoaded", async () => {
         div.appendChild(desc);
         div.appendChild(foot);
 
+        // sauvegarde des divisions dans le storage interne du navigateur.
+        result.push({footer: footer[0].value, title: titre[0].value, desc: area[0].value.replace(/\n/g, "<br/>")});
+        saveResult();
+
         // ajout de la division créée à la division "se".
         se.appendChild(div);
         titre[0].value = "";
         area[0].value = "";
         footer[0].value = "";
     })
+
+    chargeResult(se);
 })
+
+function saveResult() {
+    let json = JSON.stringify(result);
+    localStorage.setItem("list", json);
+}
+
+function chargeResult(se) {
+    let json = localStorage.getItem("list");
+    if(json === null || json === undefined) {
+        localStorage.setItem("list", JSON.stringify(result));
+    }
+    let res = JSON.parse(json);
+    if(res) {
+        for(const item of res) {
+            // création et ajout d'élément à une nouvelle division.
+            let div = document.createElement("div");
+            let title = document.createElement("h3");
+            title.innerHTML = item.title
+            let desc = document.createElement("p");
+            desc.innerHTML = item.desc
+            let foot = document.createElement("p");
+            foot.innerHTML = item.footer
+
+            // ajout des éléments à la division créée.
+            div.appendChild(title);
+            div.appendChild(desc);
+            div.appendChild(foot);
+
+            // ajout de la division créée à la division "se".
+            se.appendChild(div);
+
+            result.push(item);
+        }
+    }
+}
